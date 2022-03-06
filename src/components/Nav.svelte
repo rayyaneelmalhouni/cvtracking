c7b162604bf34c6609f6b1a9de3bdb41b023407e
RayyaneElmalhouni <elmalhounirayyane@gmail.com>
2022-03-06 16:31:13 +0100

    Got nav is working

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,3 +1,37 @@
 <script>
+	let isOpen = false;
 	export let segment;
+	function toggleNav() {
+		isOpen = !isOpen;
+	}
 </script>
+
+<style>
+	@media (min-width: 769px) {
+		nav {
+			border-bottom: solid gray 1px;
+		}
+	}
+</style>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+	<div class="navbar-brand">
+	<a href="/" class="navbar-item">
+		<img src="logo.jpg" alt="virus">
+	</a>
+	<span class="navbar-burger" 
+	aria-label="menu" 
+	aria-expanded="false" 
+	on:click={toggleNav}
+	class:is-active={isOpen}>
+	<span aria-hidden="true" />
+	<span aria-hidden="true" />
+	<span aria-hidden="true" />
+	</span>
+	</div>
+	<div class="navbar-menu" class:is-active={isOpen}>
+		<div class="navbar-start">
+			<a href="/" class="navbar-item" class:is-active={segment === undefined}>Home</a>
+			<a href="/about" class="navbar-item" class:is-active={segment === "about"}>About</a>
+		</div>
+	</div>
+</nav>	
